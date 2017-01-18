019eef5940a97a72201b4d8ca0f579313902934b
aashwin <aashwin.varshney@gmail.com>
2017-01-18 22:10:04 +1100

    Back on track, with new setup function

--- enigma.py
+++ enigma.py
@@ -7,39 +7,25 @@ import string
 from collections import namedtuple
 
 Rotor = namedtuple('Rotor', ['wires_forward', 'wires_backward', 'notch'])
-enigma = {} # this is the machine
+enigma = {}  # this is the machine
 
-def inverse_dict(original):
-    inversed = {}   # inversed dict
-    for (key, value) in original.items():
-        inversed[value] = key   # reverse key and value
-    return inversed
 
-def make_json_file():
-    rotors_json = open("reflectors.json", 'r')
+def setup():
+    rotors_json = open('rotors_v2.json', 'r')
+    reflectors_json = open('reflectors_v2.json', 'r')
+
     rotors = json.load(rotors_json)
+    reflectors = json.load(reflectors_json)
 
-    for rotor_num in rotors.keys():
-        # wires forward
-        wires_forward = dict(zip(string.ascii_uppercase, list(rotors[rotor_num]['wires'])))
-        wires_reverse = inverse_dict(wires_forward) # inverse the dictionary
-
-        enigma[rotor_num] = {}
-        enigma[rotor_num]['wires'] = wires_forward
-
-    new_json = open("reflectors_v2.json", 'w')
-    json.dump(enigma, new_json)
-    print("done!")
-
+    enigma['rotors'] = rotors
+    enigma['reflectors'] = reflectors
 
 
 # read text from user
 # print out converted text from map
 
 if __name__ == "__main__":
-    make_json_file()
-    for num in enigma.keys():
-        print(num)
-#TODO: with 3 rotors
-#TODO: with reflector
-#TODO: with inverse encryption
+    setup()
+# TODO: with 3 rotors
+# TODO: with reflector
+# TODO: with inverse encryption
