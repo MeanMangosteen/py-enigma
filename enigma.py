c806136dc3010d845de8e49c462e8653d4e0df79
aashwin <aashwin.varshney@gmail.com>
2017-01-19 00:38:49 +1100

    Rotors mapping: forward and reverse working. No stepping mechanism.

--- enigma.py
+++ enigma.py
@@ -6,7 +6,6 @@ import json
 import string
 from collections import namedtuple
 
-Rotor = namedtuple('Rotor', ['wires_forward', 'wires_backward', 'notch'])
 enigma = {}  # this is the machine
 
 
@@ -22,10 +21,26 @@ def setup():
 
 
 # read text from user
+def rotor_encrypt(letter, rotor_list, reverse=False):
+    for rotor in rotor_list:
+        if reverse:
+            rotor_map = enigma['rotors'][rotor]['wires_reverse']
+        else:
+            rotor_map = enigma['rotors'][rotor]['wires_forward']
+        # update the letter to encrypt for next rotor
+        letter = rotor_map[letter]
+        print(letter)
+    # letter is now e
+    return letter
+
+
 # print out converted text from map
 
 if __name__ == "__main__":
     setup()
+    a = rotor_encrypt('A', ['III', 'II', 'I'] )
+    print("now revesring")
+    a = rotor_encrypt('T', ['I', 'II', 'III'], reverse=True)
 # TODO: with 3 rotors
 # TODO: with reflector
 # TODO: with inverse encryption
