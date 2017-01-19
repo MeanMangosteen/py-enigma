333820504f37e5d92f406a0e8a8600d2077b39e7
aashwin <aashwin.varshney@gmail.com>
2017-01-19 18:00:54 +1100

    most skeleton of turn_rotors. shift_letter written and working.

--- enigma.py
+++ enigma.py
@@ -6,6 +6,7 @@ import json
 
 enigma = {}  # this is the machine
 
+
 def setup():
     rotors_json = open('rotors_v2.json', 'r')
     reflectors_json = open('reflectors_v2.json', 'r')
@@ -20,7 +21,10 @@ def setup():
 
 
 # read text from user
+# TODO: should take in list of rotor objects not rotor strings
 def rotor_encrypt(letter, rotor_list, reverse=False):
+    # rightmost rotor is turned before encryption
+    turn_rotor(rotor_list[-1])
     # reverse the 'rotor_list', letter goes
     # through last rotor first in forward direction
     if not reverse:
@@ -37,15 +41,50 @@ def rotor_encrypt(letter, rotor_list, reverse=False):
     # letter is now e
     return letter
 
+
+# TODO: takes a list of rotor objects
+def turn_rotors(rotor_list):
+    last_element = len(rotor_list) - 1
+    first_element = 0
+    # set all all rotors won't turn
+    for rotor in rotor_list:
+        rotor['turn'] = False;
+    rotor_list[-1]['turn'] = True  # rightmost rotor always turns
+    for i in range(last_element, first_element, -1):  # '-1' mean negative step
+        current_rotor = rotor_list[i]
+        rotor_leftof = rotor_list[i - 1]  # the rotor leftof current rotor
+        # turn both current rotor and one to left if in notch position
+        if current_rotor['position'] == current_rotor['notch']:
+            current_rotor['turn'] = True
+            rotor_leftof['turn'] = True
+
+    for rotor in rotor_list:
+        if rotor['turn']:
+            # turn the rotor letter by one
+            rotor['position'] = chr(ord(rotor['position']) + 1)
+            # return
+
+# TODO: throw exception if letter not with 'A' and 'Z'
+def shift_letter(letter, num_shifts):
+    letter = chr(ord(letter) + num_shifts)
+    if ord(letter) > ord('Z'):  # wrap back around 'A' if greater than 'Z'
+        letter = chr(ord('A') + ord(letter) - ord('Z') - 1)
+    elif ord(letter) < ord('A'):  # wrap back around 'Z' if less than 'A'
+        letter = chr(ord('Z') + ord(letter) - ord('A') + 1)
+    return letter
+
+
 def set_ring_setting():
     for rotor in enigma['rotors'].keys():
         rotor['setting'] = 'A'
         rotor['position'] = 'A'
+
+
 # print out converted text from map
 
 if __name__ == "__main__":
     setup()
-    a = rotor_encrypt('A', ['III', 'II', 'I'] )
+    a = rotor_encrypt('A', ['III', 'II', 'I'])
     print("now revesring")
     a = rotor_encrypt('T', ['I', 'II', 'III'], reverse=True)
 # TODO: with 3 rotors
